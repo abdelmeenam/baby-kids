2b2df6fb4943505635c4f828b4d0bb2363ba9d2b
abdelmeenam <abdelmeenam@gmail.com>
2022-11-01 22:35:17 +0200

    update relations

--- app/Http/Controllers/User/TeacherController.php
+++ app/Http/Controllers/User/TeacherController.php
@@ -10,16 +10,18 @@ use Illuminate\Http\Request;
 class TeacherController extends Controller
 {
     public function teacher(){
-        //$courses = Course::with('teacher')->get();
+        //-----------One to one----------
         //$teacher = Teacher::with('course')->first();
         //dd($teacher->course->name);
-        // dd($teacher);
+        //dd($teacher);
 
-        //many to many
-        $course  = Course::with('allTeachers')->first();
+        $courses = Course::with('teacher:id,name,course_id')->select(['id' , 'name' , 'price']) ->first();
+        dd($courses);
+        //------------many to many-------
+        //$course  = Course::with('allTeachers')->first();
         //dd($course->allTeachers);
-        foreach ($course->allTeachers as $teacher){
-            dump ($teacher->name);
-        }
+//        foreach ($course->allTeachers as $teacher){
+//            dump ($teacher->name);
+//        }
     }
 }
